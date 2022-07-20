a2f452b5aa4e446b1c5cb9bfb069e61cda0ddc83
narakarax <liliyaogerenko@gmail.com>
2022-07-20 00:09:55 -0700

    APP-[7201209]

--- src/data/about.js
+++ src/data/about.js
@@ -12,9 +12,9 @@ const education = [
 const hobbies = ["Fitness", "Guitar", "Video Games", "TV, Movies, & Music"];
 
 const myJourneyParagraphs = [
-  "I'm professional web developer in creating website development for companies/industries, health care, management system.",
-  "Highly experienced in front-end / back-end development. My major responsibility was to make E-Commerce sites landing pages using React, Next.js, Vue.js, React Native and Tailwind CSS. In addition to this, there was backend development using Laravel, Django and Magento.",
-  "Also, I am well versed and have worked extensively in the following technologies/areas:Python AI function, React Redux, Hooks, SSR, SSA, C#, C++, .NET, Xamarin and Decision Trees.",
+  "I'm professional web developer in creating website development for companies/industries, health care, management system and NFT marketplace/minting site.",
+  "Highly experienced in front-end / back-end / BlockChain development. My major responsibility was to make E-Commerce sites landing pages / NFT Website using React, Next.js, Vue.js, React Native, Tailwind CSS and Smart Contract. In addition to this, there was backend development using Laravel, Django and Magento.",
+  "Also, I am well versed and have worked extensively in the following technologies/areas:Python AI function, React Redux, Hooks, SSR, SSA, C#, C++, .NET, Solana, Solidity, Rust, Xamarin and Decision Trees.",
   "Web development quickly become my primary focus and passion, where I continue learning every day and seeking out new challenges."
 ];
 
