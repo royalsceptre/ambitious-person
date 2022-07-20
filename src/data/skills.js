98936e3437a360d24b69435070cc8c93e0528530
narakarax <liliyaogerenko@gmail.com>
2022-07-19 23:08:48 -0700

    complete

---
+++ src/data/skills.js
@@ -0,0 +1,38 @@
+const languages = ["Java", "Javascript", "TypeScript", "C#"];
+
+const frontEnd = [
+  "React / Next.js / React Native",
+  "Vue.js / Vuetify",
+  "Shopify / WordPress/ Elementor",
+  "Tailwind CSS / SCSS",
+  "Three.js fiber / Drei",
+];
+
+const backEnd = ["Node / Laravel / WP", "MongoDB / PostgreSQL / MYSQL", "Express / RESTFul API", "Solidity / Rust (Axum)", "Solana"];
+
+const tools = [
+  "VS Code",
+  "Visual Studio",
+  "Git / Github",
+  "Powershell / Bash",
+  "AWS / CloudFlare / Oracle / Vercel"
+];
+
+const multimedia = [
+  "Photoshop",
+  "Illustrator",
+  "Premiere",
+  "After Effects",
+  "Maya"
+];
+
+const otherSkills = ["Jest", "Babel / Webpack", "Story Book / Emulator", "NPM/YARN"];
+
+export default {
+  languages,
+  frontEnd,
+  backEnd,
+  tools,
+  multimedia,
+  otherSkills
+};
